86bb7395e124ad8e3ba8b2fdfec982caff26aaae
mirkojp <mirkopavan88878@gmail.com>
2025-04-08 15:49:46 -0300

    subir a render

--- redis_mandalorian_project/settings.py
+++ redis_mandalorian_project/settings.py
@@ -82,7 +82,13 @@ WSGI_APPLICATION = 'redis_mandalorian_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
+DATABASES = {
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
 
 
 # Password validation
