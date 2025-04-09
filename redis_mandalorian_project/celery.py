85a3ddb141a7f81dc7485d72ad615524b8f157dd
mirkojp <mirkopavan88878@gmail.com>
2025-04-09 14:17:00 -0300

    add redis

--- redis_mandalorian_project/celery.py
+++ redis_mandalorian_project/celery.py
@@ -1,8 +1,9 @@
-import os
 from celery import Celery
+import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redis_mandalorian_project.settings")
 
 app = Celery("redis_mandalorian_project")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.broker_url = os.getenv("REDIS_URL")  # este apunta a Upstash
 app.autodiscover_tasks()
